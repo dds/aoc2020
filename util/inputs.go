f7e52deb33fc2199f1d66b656e928634c7150a94
David Smith <dsmith@nerdwallet.com>
2020-11-22 15:51:54 -0800

    util: rename Input to ParseInput and provide raw inputs

--- util/inputs.go
+++ util/inputs.go
@@ -7,15 +7,15 @@ import (
 )
 
 // All puzzle inputs stored as an array of UTF-8 strings.
-var inputs []string
+var Inputs []string
 
 // Returns the input for the given day, each line of the input parsed by the
 // parser function into strings. Panics on error.
-func Input(day int, parser func(string) []string) [][]string {
-	if day > len(inputs) {
+func ParseInput(day int, parser func(string) []string) [][]string {
+	if day > len(Inputs) {
 		panic(fmt.Errorf("no input for day %v", day))
 	}
-	lines := strings.Split(inputs[day], "\n")
+	lines := strings.Split(Inputs[day], "\n")
 	r := make([][]string, 0)
 	for _, line := range lines {
 		fields := parser(line)
@@ -30,7 +30,7 @@ func Input(day int, parser func(string) []string) [][]string {
 
 // Returns the input as a two-dimensional array of float64.
 func InputFloats(day int, parser func(string) []string) [][]float64 {
-	lines := Input(day, parser)
+	lines := ParseInput(day, parser)
 
 	r := make([][]float64, len(lines))
 	var err error
@@ -70,8 +70,8 @@ func CSVParser(input string) []string {
 }
 
 func init() {
-	inputs = make([]string, 25)
-	inputs[0] = `1,2,3
+	Inputs = make([]string, 25)
+	Inputs[0] = `1,2,3
 4,5,6
 7,8,9,10
 `
