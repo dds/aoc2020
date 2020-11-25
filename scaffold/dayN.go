f7bdb7d9644ddf8224206c7ca6235add8d3ba926
David Smith <dsmith@nerdwallet.com>
2020-11-25 10:06:12 -0800

    move test file into scaffold file

--- scaffold/dayN.go
+++ scaffold/dayN.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
+	"testing"
 
 	"github.com/dds/aoc2020/util"
 )
 
 var Input = util.Inputs[0]
+
+func Test(t *testing.T) {
+	// type test struct {
+	// 	input  int
+	// 	expect int
+	// }
+
+	// tests := []test{
+	// 	test{
+	// 		// ...
+	// 	},
+	// }
+
+	// for i, test := range tests {
+	// 	t.Run(fmt.Sprint(i), func(t *testing.T) {
+	// 		require.Equal(t, test.expect, test.input)
+	// 	})
+	// }
+}
 
 func main() {
 	fmt.Println(part1(Input))
