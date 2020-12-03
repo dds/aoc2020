fee7b48b4a6307468bb06eeeb20295de542ce401
David D. Smith <dsmith@nerdwallet.com>
2020-12-03 01:24:28 -0800

    add some skiers and snowboards

--- day3/day3.go
+++ day3/day3.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"image"
+	"math/rand"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/dds/aoc2020/lib"
 	"github.com/dds/aoc2020/lib/inputs"
+	"github.com/gdamore/tcell"
 )
 
 var Input = lib.ParseInput(inputs.Day3(), func(s string) []string { return strings.Split(s, "") })
@@ -34,6 +37,7 @@ func Test(t *testing.T) {
 func main() {
 	fmt.Println(part1(Input))
 	fmt.Println(part2(Input))
+	shred(Input)
 }
 
 func part1(input [][]string) (rc int) {
@@ -72,4 +76,153 @@ func part2(input [][]string) (rc int) {
 		rc *= c
 	}
 	return
+}
+
+const (
+	trail     = '╍'
+	skier     = '⛷'
+	snowbdr   = '🏂'
+	tree      = '🌲'
+	mntn      = '🏔'
+	snowman   = '⛄'
+	xmastree  = '🎄'
+	snowflake = '❄'
+)
+
+func background(s tcell.Screen) {
+	colors := []tcell.Color{
+		//  - ffffff 80%
+		//  - fdfdfd 10%
+		//  - fefefe 10%
+		tcell.NewRGBColor(0xfe, 0xfe, 0xfe),
+		tcell.NewRGBColor(0xfd, 0xfd, 0xfd),
+		tcell.NewRGBColor(0xfb, 0xfb, 0xfb),
+	}
+	w, h := s.Size()
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			p := rand.Int() % 100
+			idx := 2
+			if p < 80 {
+				idx--
+			}
+			if p < 90 {
+				idx--
+			}
+			_, _, style, _ := s.GetContent(x, y)
+			s.SetContent(x, y, ' ', nil, style.Background(colors[idx]))
+		}
+	}
+}
+
+func foreground(s tcell.Screen, scene int, input [][]string) {
+	w, h := s.Size()
+	m := len(input[0])
+	// Trees
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			if input[(y+scene)%len(input)][x%m] == "#" {
+				_, _, style, _ := s.GetContent(x, y)
+				s.SetContent(x, y, tree, nil, style)
+			}
+		}
+	}
+	// Riders
+	type rider struct {
+		image.Point
+		tcell.Color
+		glyph rune
+	}
+	riders := map[image.Point]rider{
+		image.Point{1, 1}: rider{
+			glyph: skier,
+			//   - ff8352 // orange
+			Color: tcell.NewRGBColor(0xFF, 0x83, 0x52),
+		},
+		image.Point{3, 1}: rider{
+			glyph: snowbdr,
+			//   - ffb71c // gold
+			Color: tcell.NewRGBColor(0xFF, 0xB7, 0x1C),
+		},
+		image.Point{5, 1}: rider{
+			glyph: skier,
+			//   - ff461c // red
+			Color: tcell.NewRGBColor(0xFF, 0x46, 0x1c),
+		},
+		image.Point{7, 1}: rider{
+			glyph: skier,
+			//   - 91ff1c // neon green
+			Color: tcell.NewRGBColor(0x91, 0xFF, 0x1C),
+		},
+		image.Point{1, 2}: rider{
+			glyph: snowbdr,
+			//   - ff1c7b // pink
+			Color: tcell.NewRGBColor(0xFF, 0x1C, 0x7B),
+		},
+	}
+	for slope, rider := range riders {
+		// Update the trail along the slope and put rider at the end.
+		rider.Point.Y = scene
+		rider.Point.X = scene * slope.X
+		_, _, style, _ := s.GetContent(rider.Point.X, rider.Point.Y)
+		s.SetContent(rider.Point.X, rider.Point.Y, rider.glyph, nil, style.Foreground(rider.Color))
+	}
+}
+
+func shred(input [][]string) {
+	sc, err := tcell.NewScreen()
+	if err != nil {
+		panic(err)
+	}
+	if err := sc.Init(); err != nil {
+		panic(err)
+	}
+	sc.Clear()
+	userquit := make(chan struct{})
+	go func() {
+		for {
+			switch ev := sc.PollEvent().(type) {
+			case *tcell.EventResize:
+				sc.Sync()
+			case *tcell.EventKey:
+				if ev.Key() == tcell.KeyEscape {
+					userquit <- struct{}{}
+				}
+			}
+		}
+	}()
+	defer func() {
+		sc.Fini()
+		if err := recover(); err != nil {
+			panic(err)
+		}
+	}()
+
+	var (
+		clock = time.Second / 10
+		timer = time.NewTimer(clock)
+		scene = 0
+	)
+
+	// Loop:
+	//  - reset the scene timer
+	//  - paint background
+	//  - update riders and trails
+	//  - pause for refresh or cancel
+	//  - display the screen
+	//  - increment the row
+loop:
+	for scene < len(input) {
+		timer.Reset(clock)
+		background(sc)
+		foreground(sc, scene, input)
+		sc.Show()
+		select {
+		case <-userquit:
+			break loop
+		case <-timer.C:
+		}
+		scene++
+	}
+	sc.Fini()
 }
