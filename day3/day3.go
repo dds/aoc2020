2420caad77bc24c530abc3c6f4ccd1d0bbe0c1e0
David D. Smith <dsmith@nerdwallet.com>
2020-12-02 22:04:49 -0800

    day3: parts 1 and 2

--- day3/day3.go
+++ day3/day3.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"strings"
 	"testing"
 
@@ -36,10 +37,39 @@ func main() {
 }
 
 func part1(input [][]string) (rc int) {
-	fmt.Println(input)
+	x := 0
+	for y, row := range input {
+		n := len(row)
+		if row[x%n] == "#" {
+			rc++
+		}
+		x += 3
+	}
 	return
 }
 
 func part2(input [][]string) (rc int) {
+	counters := map[image.Point]int{
+		image.Point{1, 1}: 0,
+		image.Point{3, 1}: 0,
+		image.Point{5, 1}: 0,
+		image.Point{7, 1}: 0,
+		image.Point{1, 2}: 0,
+	}
+	for y, row := range input {
+		n := len(row)
+		for p := range counters {
+			if p.Y == 2 && y%2 == 1 {
+				continue
+			}
+			if row[y/p.Y*p.X%n] == "#" {
+				counters[p]++
+			}
+		}
+	}
+	rc = 1
+	for _, c := range counters {
+		rc *= c
+	}
 	return
 }
