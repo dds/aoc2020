5a0439ec3204035f7817d961eaa4a446004d2537
David D. Smith <dsmith@nerdwallet.com>
2020-12-03 20:10:53 -0800

    lib: separate Neighbors4 from UDLR

--- lib/grid.go
+++ lib/grid.go
@@ -23,3 +23,29 @@ func Taxi(p, q image.Point) float64 {
 	r := image.Rectangle{p, q}.Canon()
 	return float64(r.Dx() + r.Dy())
 }
+
+var directions = []image.Point{
+	image.Point{0, 1},
+	image.Point{0, -1},
+	image.Point{-1, 0},
+	image.Point{1, 0},
+}
+
+// Returns the neighbors up, down, left, and right of P.
+func Neighbors4(p image.Point) (r []image.Point) {
+	for _, q := range directions {
+		r = append(r, p.Add(q))
+	}
+	return
+}
+
+// Returns 1, 2, 3, or 4 if q is the up, down, left, or right neighbor of P, or
+// 0 otherwise.
+func UDLR(p, q image.Point) int {
+	for i, u := range directions {
+		if q.Sub(p) == u {
+			return i + 1
+		}
+	}
+	return 0
+}
