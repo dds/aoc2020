c7bca90fca886788d2f3a8ba6a26d6e3df97645a
David D. Smith <davidsmith@acm.org>
2020-12-05 21:50:51 -0800

    day6: parts 1 and 2

--- day6/day6.go
+++ day6/day6.go
@@ -2,13 +2,64 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 	"testing"
 
 	"github.com/dds/aoc2020/lib"
 	"github.com/dds/aoc2020/lib/inputs"
 )
 
-var Input = lib.InputInts(inputs.TestInput1(), lib.NumberParser)
+func parse(input string) [][]string {
+	lines := strings.Split(input, "\n\n")
+	r := make([][]string, 0)
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if len(fields) == 0 {
+			continue
+		}
+		r = append(r, fields)
+	}
+
+	return r
+}
+
+var Input = parse(inputs.Day6())
+
+func part1(input [][]string) (rc int) {
+	for _, r := range input {
+		m := map[rune]int{}
+		for _, c := range strings.Join(r, "") {
+			m[c]++
+		}
+		rc += len(m)
+	}
+	return
+}
+
+func part2(input [][]string) (rc int) {
+	for _, r := range input {
+		m := map[rune]int{}
+		sortedAnswers := []string{}
+		minLength := 1 << 31
+		for _, c := range r {
+			minLength = lib.Min(len(c), minLength)
+			s := strings.Split(c, "")
+			sort.Strings(s)
+			for _, k := range s {
+				m[rune(k[0])]++
+			}
+			sortedAnswers = append(sortedAnswers, strings.Join(s, ""))
+		}
+		first := sortedAnswers[0]
+		for _, c := range first {
+			if m[c] == len(r) {
+				rc++
+			}
+		}
+	}
+	return
+}
 
 func Test(t *testing.T) {
 	// type test struct {
@@ -32,13 +83,4 @@ func Test(t *testing.T) {
 func main() {
 	fmt.Println(part1(Input))
 	fmt.Println(part2(Input))
-}
-
-func part1(input [][]int) (rc int) {
-	fmt.Println(input)
-	return
-}
-
-func part2(input [][]int) (rc int) {
-	return
 }
