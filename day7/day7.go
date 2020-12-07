0e9593d2d26fb54228b2c6c3e43b022644b97e73
David D. Smith <davidsmith@acm.org>
2020-12-06 22:37:19 -0800

    day7: parts 1 and 2

--- day7/day7.go
+++ day7/day7.go
@@ -2,43 +2,81 @@ package main
 
 import (
 	"fmt"
-	"testing"
+	"strconv"
+	"strings"
 
 	"github.com/dds/aoc2020/lib"
 	"github.com/dds/aoc2020/lib/inputs"
 )
 
-var Input = lib.InputInts(inputs.TestInput1(), lib.NumberParser)
+func parse(s string) []string {
+	parts := lib.TrimSpace(strings.Split(s, "bags contain"))
+	if len(parts) < 2 {
+		return []string{}
+	}
+	bags := strings.Split(parts[1], ", ")
+	r := []string{parts[0]}
+	// Drop the last work from each group.
+	for _, b := range bags {
+		t := strings.Fields(b)
+		r = append(r, strings.Join(t[:len(t)-1], " "))
+	}
+	return r
+}
 
-func Test(t *testing.T) {
-	// type test struct {
-	// 	input  int
-	// 	expect int
-	// }
+var Input = lib.ParseInput(inputs.Day7(), parse)
 
-	// tests := []test{
-	// 	test{
-	// 		// ...
-	// 	},
-	// }
+func part1(input [][]string) (rc int) {
+	m := map[string][]string{}
+	for _, row := range input {
+		for _, s := range row[1:] {
+			bag := s[2:]
+			m[bag] = append(m[bag], row[0])
+		}
+	}
+	bagQueue := []string{"shiny gold"}
+	seenBags := map[string]int{}
+	for len(bagQueue) > 0 {
+		bag := bagQueue[0]
+		bagQueue = bagQueue[1:]
+		for _, u := range m[bag] {
+			if seenBags[u] < 1 {
+				seenBags[u] = 1
+				bagQueue = append(bagQueue, u)
+				rc++
+			}
+		}
+	}
+	return
+}
 
-	// for i, test := range tests {
-	// 	t.Run(fmt.Sprint(i), func(t *testing.T) {
-	// 		require.Equal(t, test.expect, test.input)
-	// 	})
-	// }
+func part2(input [][]string) (rc int) {
+	type bagCount struct {
+		count int
+		bag   string
+	}
+	m := map[string][]bagCount{}
+	for _, row := range input {
+		for _, s := range row[1:] {
+			count, err := strconv.Atoi(s[:1])
+			if err != nil {
+				count = 0
+			}
+			bag := s[2:]
+			m[row[0]] = append(m[row[0]], bagCount{count: count, bag: bag})
+		}
+	}
+	var count func(map[string][]bagCount, string) int
+	count = func(m map[string][]bagCount, bag string) (sum int) {
+		for _, u := range m[bag] {
+			sum += u.count * (1 + count(m, u.bag))
+		}
+		return sum
+	}
+	return count(m, "shiny gold")
 }
 
 func main() {
 	fmt.Println(part1(Input))
 	fmt.Println(part2(Input))
-}
-
-func part1(input [][]int) (rc int) {
-	fmt.Println(input)
-	return
-}
-
-func part2(input [][]int) (rc int) {
-	return
 }
