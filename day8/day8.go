36dca0e26bcef0e936cf454b805c07cf5cffe8ad
David D. Smith <dsmith@nerdwallet.com>
2020-12-08 14:39:12 -0800

    day8: serial and parallel implementation and benchmarks

--- day8/day8.go
+++ day8/day8.go
@@ -56,12 +56,12 @@ func part1(input [][]string) (rc int) {
 	}
 }
 
-func run(instructions map[int]op) (bool, int) {
+func run(instructions []op) (bool, int) {
 	st := state{}
 
 	seenJumps := map[int]int{}
 	for {
-		if st.stack == len(instructions) {
+		if st.stack >= len(instructions) {
 			return true, st.accumulator
 		}
 		i := instructions[st.stack]
@@ -77,39 +77,71 @@ func run(instructions map[int]op) (bool, int) {
 			}
 			seenJumps[st.stack] = 1
 			st.stack += i.arg
+		default:
+			panic(fmt.Errorf("unknown operation %v", i))
 		}
 	}
 }
 
-func part2(input [][]string) (rc int) {
-	instructions := map[int]op{}
+func part2_serial(input [][]string) (rc int) {
+	instructions := make([]op, len(input))
 	for lineNo, row := range input {
 		arg, _ := strconv.Atoi(row[1])
 		instructions[lineNo] = op{inst: row[0], arg: arg}
 	}
 
 	for k, v := range instructions {
-		orig := instructions[k]
+		ins := make([]op, len(instructions))
+		copy(ins, instructions)
 		switch v.inst {
 		case "nop":
-			instructions[k] = op{inst: "jmp", arg: v.arg}
+			ins[k] = op{inst: "jmp", arg: v.arg}
 		case "jmp":
-			instructions[k] = op{inst: "nop", arg: v.arg}
+			ins[k] = op{inst: "nop", arg: v.arg}
 		default:
 			continue
 		}
-		res, acc := run(instructions)
+		res, acc := run(ins)
 		if res {
 			return acc
 		}
-		instructions[k] = orig
 	}
 	return
+}
+
+func part2_parallel(input [][]string) (rc int) {
+	instructions := make([]op, len(input))
+	for lineNo, row := range input {
+		arg, _ := strconv.Atoi(row[1])
+		instructions[lineNo] = op{inst: row[0], arg: arg}
+	}
+
+	result := make(chan int)
+
+	for k, v := range instructions {
+		go func(k int, v op) {
+			ins := make([]op, len(instructions))
+			copy(ins, instructions)
+			switch v.inst {
+			case "nop":
+				ins[k] = op{inst: "jmp", arg: v.arg}
+			case "jmp":
+				ins[k] = op{inst: "nop", arg: v.arg}
+			default:
+				return
+			}
+			res, acc := run(ins)
+			if res {
+				result <- acc
+			}
+		}(k, v)
+	}
+	return <-result
 }
 
 var Input = lib.ParseInput(inputs.Day8(), parse)
 
 func main() {
 	fmt.Println(part1(Input))
-	fmt.Println(part2(Input))
+	fmt.Println(part2_serial(Input))
 }
