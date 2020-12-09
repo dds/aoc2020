335fdf6a0f3043f0ec538a2d8c870c957b46df28
David D. Smith <davidsmith@acm.org>
2020-12-08 21:26:51 -0800

    day9: parts 1 and 2

--- day9/day9.go
+++ day9/day9.go
@@ -2,43 +2,72 @@ package main
 
 import (
 	"fmt"
-	"testing"
 
 	"github.com/dds/aoc2020/lib"
 	"github.com/dds/aoc2020/lib/inputs"
 )
 
-var Input = lib.InputInts(inputs.TestInput1(), lib.NumberParser)
-
-func Test(t *testing.T) {
-	// type test struct {
-	// 	input  int
-	// 	expect int
-	// }
-
-	// tests := []test{
-	// 	test{
-	// 		// ...
-	// 	},
-	// }
-
-	// for i, test := range tests {
-	// 	t.Run(fmt.Sprint(i), func(t *testing.T) {
-	// 		require.Equal(t, test.expect, test.input)
-	// 	})
-	// }
+func parse(input [][]int) (r []int) {
+	for _, row := range input {
+		r = append(r, row[0])
+	}
+	return
 }
 
+var Input = parse(lib.InputInts(inputs.Day9(), lib.NumberParser))
+
+func validNumbers(in []int, preambleSize int) (r []int) {
+	if len(in) < preambleSize {
+		return
+	}
+	for i := 0; i < preambleSize-1; i++ {
+		for j := i + 1; j < preambleSize; j++ {
+			r = append(r, in[i]+in[j])
+		}
+	}
+	return
+}
+
+func contains(in []int, needle int) bool {
+	for _, i := range in {
+		if i == needle {
+			return true
+		}
+	}
+	return false
+}
+
+func part1(input []int, preambleSize int) (rc int) {
+	for i := preambleSize; i < len(input); i++ {
+		candidates := validNumbers(input[i-preambleSize:], preambleSize)
+		if !contains(candidates, input[i]) {
+			return input[i]
+		}
+	}
+	return
+}
+
+func part2(input []int, preambleSize int) (rc int) {
+	target := part1(input, preambleSize)
+loop:
+	for i := 0; i < len(input)-1; i++ {
+		max, min, sum := 0, 1<<31, 0
+		for j := i; j < len(input); j++ {
+			t := input[j]
+			sum += t
+			if sum > target {
+				continue loop
+			}
+			min = lib.Min(min, t)
+			max = lib.Max(max, t)
+			if sum == target {
+				return min + max
+			}
+		}
+	}
+	return
+}
 func main() {
-	fmt.Println(part1(Input))
-	fmt.Println(part2(Input))
-}
-
-func part1(input [][]int) (rc int) {
-	fmt.Println(input)
-	return
-}
-
-func part2(input [][]int) (rc int) {
-	return
+	fmt.Println(part1(Input, 25))
+	fmt.Println(part2(Input, 25))
 }
---
+++ day9/day9_test.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPart1(t *testing.T) {
+	type test struct {
+		input  []int
+		expect int
+	}
+
+	tests := []test{
+		test{
+			input: []int{
+				35,
+				20,
+				15,
+				25,
+				47,
+				40,
+				62,
+				55,
+				65,
+				95,
+				102,
+				117,
+				150,
+				182,
+				127,
+				219,
+				299,
+				277,
+				309,
+				576,
+			},
+			expect: 127,
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			require.Equal(t, test.expect, part1(test.input, 5))
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	type test struct {
+		input  []int
+		expect int
+	}
+
+	tests := []test{
+		test{
+			input: []int{
+				35,
+				20,
+				15,
+				25,
+				47,
+				40,
+				62,
+				55,
+				65,
+				95,
+				102,
+				117,
+				150,
+				182,
+				127,
+				219,
+				299,
+				277,
+				309,
+				576,
+			},
+			expect: 62,
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			require.Equal(t, test.expect, part2(test.input, 5))
+		})
+	}
+}
